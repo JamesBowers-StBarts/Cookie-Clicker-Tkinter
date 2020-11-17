c82406874ed98159e6946c9418d0d50a1a136528
DemonicDrake <61542853+DemonicDrake@users.noreply.github.com>
2020-11-17 09:51:38 +0000

    Update Cookie Clicker.py

--- Cookie Clicker.py
+++ Cookie Clicker.py
@@ -1,36 +1,69 @@
 from tkinter import *
-from PIL import ImageTk, Image
 
+add_per_second=1
 
+def move_frame(direction_number):
+    global current_frame
+    current_frame = current_frame + direction_number
+    if current_frame == 2:
+        current_frame =0
+    elif current_frame == -1:
+        current_frame =1
+    selected_frame = frames_list[current_frame]
+    selected_frame.tkraise()
 
-def change_visibility(frame_parameter):
-    frame_parameter.tkraise()
+def upgrade_cookie_growth():
+    global add_per_second
+    global no_of_cookies
+    if no_of_cookies>=50:
+        add_per_second=add_per_second+1
+        no_of_cookies=no_of_cookies-50
+    else:
+        print()
 
+def Add_cookies():
+    global no_of_cookies
+    global add_per_second
+    no_of_cookies = no_of_cookies + add_per_second
+    cookies_label.configure(text="cookies: "+str(no_of_cookies))
+    cps_label.configure(text="CPS: "+str(add_per_second))
+    root.after(1000, Add_cookies)
+
+def Manual_cookie_add():
+    global no_of_cookies
+    no_of_cookies=no_of_cookies+1
+    cookies_label.configure(text="cookies: "+str(no_of_cookies))
 
 root =Tk()
 
-
 top_frame = Frame(root, bg = "Grey")
 top_frame.grid(row=0,column=0,sticky=E+W+S+N)
+cookies_label = Label(top_frame,text="hi")
+cookies_label.grid(row=1,column=0)
+cps_label = Label(top_frame,text="temp")
+cps_label.grid(row=1, column=1)
+no_of_cookies = 0
+Add_cookies()
 
 content_frame2 = Frame(root, bg = "white")
 content_frame2.grid(row=1,column=0,sticky=E+W+S+N)
+title = Label(content_frame2, text="Upgrades")
+upgrade_cookie_growth_button=Button(content_frame2, text="+1 cps (-50)",command=upgrade_cookie_growth)
+upgrade_cookie_growth_button.grid(row=2)
+title.grid(row=1)
 
-content_frame1 = Frame(root, bg = "white")
+content_frame1 = Frame(root, bg = "black")
 content_frame1.grid(row=1,column=0,sticky=E+W+S+N)
+manual_cookie_add_button=Button(content_frame1,text="Make a cookie",command=Manual_cookie_add)
+manual_cookie_add_button.grid(row=1)
 
-frame1_button = Button(top_frame, text="Show mole",bg = "Grey",fg="white",command = lambda: change_visibility(content_frame1))
-frame1_button.grid(row=0,column=0,pady=(10,10),sticky="ESNW")
-frame2_button = Button(top_frame, text="Show crocodile",bg = "Grey",fg="white",command = lambda: change_visibility(content_frame2))
-frame2_button.grid(row=0,column=1,pady=(10,10),sticky="ESNW")
+forward_button = Button(top_frame, text="<--",bg = "Grey",fg="white",command = lambda: move_frame(1))
+forward_button.grid(row=0,column=0,pady=(10,10),sticky="ESNW")
+back_button = Button(top_frame, text="-->",bg = "Grey",fg="white",command = lambda: move_frame(-1))
+back_button.grid(row=0,column=1,pady=(10,10),sticky="ESNW")
 
-#images will only work after Tk() has been called
-image_file = Image.open(crock)
-image_file = image_file.resize((100, 100), Image.ANTIALIAS)
-image1= ImageTk.PhotoImage(image_file)
-
-bottom_label = Label(content_frame1, image=image1,bg = "white",)
-bottom_label.grid(row=1,column=0,columnspan=2,pady=(10,10))
+current_frame = 0
+frames_list = [content_frame1,content_frame2]
 
 root.rowconfigure(0,minsize = 100,weight=0)
 root.rowconfigure(1,minsize = 400,weight=1)
@@ -39,5 +72,7 @@ root.columnconfigure(0,minsize = 500,weight=1)
 
 content_frame1.columnconfigure(0,minsize = 200,weight=1)
 content_frame1.rowconfigure(0,minsize = 200)
+content_frame2.columnconfigure(0,minsize = 200,weight=1)
 
 root.mainloop()
+
